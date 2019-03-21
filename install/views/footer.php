f3d3cb819c1eca436f81755cff1f1de6c42b4218
Alexander Isik <isik.alexander@yandex.ru>
2019-03-21 14:02:58 +1000

    Create footer.php

---
+++ install/views/footer.php
@@ -0,0 +1,8 @@
+            </header>
+        </div>
+    </div>
+    <script src="/public/assets/js/pagePreloadEffectClassie.js"></script>
+	<script src="/public/assets/js/pagePreloadEffectPathLoader.js"></script>
+	<script src="/public/assets/js/pagePreloadEffectMain.js"></script>
+</body>
+</html>
