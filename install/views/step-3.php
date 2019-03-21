6d2a02adf90d09f7a59258f6e49278b41c9b9eee
Alexander Isik <isik.alexander@yandex.ru>
2019-03-21 14:07:08 +1000

    Create step-3.php

---
+++ install/views/step-3.php
@@ -0,0 +1,4 @@
+                <h1 class="content__header___title">Готово!</h1>
+                <h3>Установка завершена и теперь вы можете начать использование The Raice LMS.</h3>
+                <br>
+                <a href="/install/install?step=4"><button class="form__submit" type="submit" id="submit">Завершить установку</button></a>
